ae8892e22989638953af2a5c411cd0370027412b
Kamil Pietak <kpietak@agh.edu.pl>
2016-09-21 10:55:12 +0200

    sample runnable added to illustrate how to run computations with gradle

---
+++ age3-stream-agents/src/main/java/pl/edu/agh/age/compute/stream/example/SampleRunnable.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.age.compute.stream.example;
+
+/**
+ * Created by kpietak on 21.09.2016.
+ */
+public class SampleRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("SampleRunnable has been executed.");
+    }
+}
