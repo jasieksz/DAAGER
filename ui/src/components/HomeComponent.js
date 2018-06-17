41607b7e6b0d0a52ca8dc8a9acf4062917c7990a
Malgorzata Stachona <malgorzata.stachon@ovoo.pl>
2018-06-17 12:02:22 +0200

    adding modal after clicking on node

--- ui/src/components/HomeComponent.js
+++ ui/src/components/HomeComponent.js
@@ -1,22 +1,101 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'react-tabs/style/react-tabs.css';
 import '../styles/HomeComponent.css';
+import Sigma from 'react-sigma/lib/Sigma'
+import {RelativeSize} from "react-sigma";
+import RandomizeNodePositions from "react-sigma/es/RandomizeNodePositions";
+import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class HomeComponent extends Component {
+
 
     constructor(props, context) {
         super(props, context);
         this.state = {
+            nodes: [{id: "n1", label: "Alice"}, {id: "n2", label: "Rabbit"}],
+            edges: [{id: "e1", source: "n1", target: "n2"}],
+            isModalOpen: false
+            // popOversOpen: new Map()
         };
+        this.toggleModal = this.toggleModal.bind(this);
+        // this.setPopOvers();
     }
 
-    renderGraph () {
+    // setPopOvers() {
+    //     this.state.nodes.forEach( node => this.state.popOversOpen.set({"id": node.id,"isOpen": true}));
+    //     console.log(this.state.popOversOpen);
+    // }
+    //
+    // togglePopover(id) {
+    //     console.log("id: " + id);
+    //     var prevState = this.state.popOversOpen.get(id);
+    //     console.log(prevState);
+    //     this.state.popOversOpen.set(id, !prevState);
+    // }
+    //
+    // renderPopOvers() {
+    //     return (
+    //         this.state.nodes.map( i =>
+    //             <Popover placement="auto" isOpen={this.state.popOversOpen.get(i.id)} toggle={this.togglePopover(i.id)}>
+    //                 <PopoverHeader>Popover Title</PopoverHeader>
+    //                 <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem
+    //                     lacinia quam venenatis vestibulum.</PopoverBody>
+    //             </Popover>
+    //         )
+    //     );
+    // }
+
+    toggleModal() {
+        console.log(this.state.isModalOpen);
+        var prev = this.state.isModalOpen;
+        this.setState({
+            isModalOpen: !prev
+        });
+        console.log(this.state.isModalOpen);
+    }
+
+    renderModal() {
         return (
-            <div className={'homeGraph'}>GRAPH</div>
+                <Modal isOpen={this.state.isModalOpen}>
+                    <ModalHeader>Modal title  </ModalHeader>
+                    <ModalBody>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggleModal}>Do Something</Button>{' '}
+                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
         );
     }
 
-    renderInfo () {
+    getGraph() {
+        return (
+                <Sigma graph={{
+                    nodes: this.state.nodes,
+                    edges: this.state.edges
+                    }}
+                   style={{maxWidth: "inherit", height: "inherit"}}
+                   onClickNode={e => { this.toggleModal();} }
+                   onOverNode={e => console.log("Mouse over node: " + e.data.node.label)}
+                   settings={{drawEdges: true, defaultNodeColor: '#0073e6'}}>
+                <RelativeSize initialSize={8}/>
+                <RandomizeNodePositions/>
+            </Sigma>
+        );
+    }
+
+    renderGraph() {
+        return (
+            <div className={'homeGraph'}>
+                {this.renderModal()}
+                {this.getGraph()}
+            </div>
+        );
+    }
+
+    renderInfo() {
         return (
             <div className={'homeInfo'}>
                 <div className={'homeInfoText'}>
--- ui/src/components/MainPage.js
+++ ui/src/components/MainPage.js
@@ -5,6 +5,10 @@ import HomeComponent from "./HomeComponent";
 import ManageComponent from "./ManageComponent";
 import GrafanaComponent from "./GrafanaComponent";
 import "../styles/MainPageComponent.css";
+import fontawesome from '@fortawesome/fontawesome'
+import faHome from '@fortawesome/fontawesome-free-solid/faHome'
+import faCog from '@fortawesome/fontawesome-free-solid/faCog'
+import faChartBar from '@fortawesome/fontawesome-free-solid/faChartBar'
 
 class MainPage extends Component {
 
@@ -27,13 +31,19 @@ class MainPage extends Component {
                 <div className={'daagerTitle'}>Daager</div>
                 <Tabs>
                     <TabList className={'tabs'}>
-                        <Tab eventKey={1} title="home">
+                        <Tab eventKey={1} title="home" >
+                            <i className={"fas fa-home fa-fw"}/>
+                            {fontawesome.library.add(faHome)}
                             Home
                         </Tab>
                         <Tab eventKey={2} title="manage">
-                           Manage
+                            <i className={"fas fa-cog fa-fw"}/>
+                            {fontawesome.library.add(faCog)}
+                            Manage
                         </Tab>
                         <Tab eventKey={3} title="grafana">
+                            <i className={"fas fa-chart-bar fa-fw"}/>
+                            {fontawesome.library.add(faChartBar)}
                             Grafana
                         </Tab>
                     </TabList>
