26245425704c1fe1c5513c744d690e870bced382
Bartosz Radzynski <bradzynski@virtuslab.com>
2018-06-19 21:43:33 +0200

    DAAGER-19 adding grafana support

--- ui/src/components/GrafanaComponent.js
+++ ui/src/components/GrafanaComponent.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'react-tabs/style/react-tabs.css';
 import '../styles/GrafanaComponent.css';
-import { Alert } from 'reactstrap';
+import {Alert} from 'reactstrap';
 
 class GrafanaComponent extends Component {
 
@@ -10,22 +10,9 @@ class GrafanaComponent extends Component {
     }
 
     render() {
-        if (this.props.pullingAddress !== '') {
-            return (
-                <div>
-                    <h2 className={'tabTitle'}>Grafana Component </h2>
-                    <div className={'grafanaLink'}>
-                        link do grafany
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <Alert color={"danger"} className={'tabTitle'}>
-                    Go to Manage Tab and set pulling address
-                </Alert>
-            );
-        }
+        return (
+                <iframe height={'100%'} width={'100%'} src="http://localhost:3001"/>
+        );
     }
 }
 
