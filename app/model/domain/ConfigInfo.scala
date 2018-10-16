09c148e8a66344ffd82fd2a34fc622ec868260b3
Malgorzata Stachona <malgorzata.stachon@ovoo.pl>
2018-10-16 19:11:40 +0200

    Creating config info service.

---
+++ app/model/domain/ConfigInfo.scala
@@ -0,0 +1,13 @@
+package model.domain
+
+import play.api.libs.json.Json
+
+case class ConfigInfo(osInterval: Int,
+                      runtimeInterval: Int,
+                      threadInterval: Int,
+                      tcpInterval: Int
+                     )
+
+object ConfigInfo {
+  implicit val format = Json.format[ConfigInfo]
+}
---
+++ app/services/ConfigInfoService.scala
@@ -0,0 +1,42 @@
+package services
+
+import akka.util.Timeout
+import javax.inject.Inject
+import model.domain.ConfigInfo
+import play.api.libs.json.Json
+import play.api.libs.ws.WSClient
+
+import scala.collection.mutable
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+class ConfigInfoService @Inject()(wsClient: WSClient) {
+
+  var intervals: mutable.HashMap[String, Int] = new mutable.HashMap[String, Int]()
+  private val timeout: Timeout = 10 seconds
+
+  def sendInitialConfigInfo(intervalValue: Int)(implicit ec: ExecutionContext): Unit = {
+    intervals.put("osInterval", intervalValue)
+    intervals.put("runtimeInterval", intervalValue)
+    intervals.put("threadInterval", intervalValue)
+    intervals.put("tcpInterval", intervalValue)
+
+    wsClient.url("/config").post(Json.toJson(ConfigInfo(intervalValue, intervalValue, intervalValue, intervalValue)))
+      .map { response =>
+        val statusText: String = response.statusText
+        println(response.body)
+      }
+  }
+
+  def sendUpdateConfigInfo(intervalValue: Int, intervalName: String)(implicit ec: ExecutionContext): Unit = {
+    intervals.put(intervalName, intervalValue)
+    wsClient.url("/config").post(
+      Json.toJson(ConfigInfo(
+        intervals("osInterval"),
+        intervals("runtimeInterval"),
+        intervals("threadInterval"),
+        intervals("tcpInterval")
+      )))
+  }
+
+}
