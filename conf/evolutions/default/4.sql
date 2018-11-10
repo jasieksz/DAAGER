dc45a56254b438cbc139745f1c9d680a1be77a7d
Malgorzata Stachona <malgorzata.stachon@ovoo.pl>
2018-11-10 12:17:10 +0100

    Adding more metrics to thread info.

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -20,10 +20,10 @@ ALTER TABLE "os_infos" DROP "disk_usable_space";
 ALTER TABLE "os_infos" DROP "disk_free_space";
 ALTER TABLE "os_infos" DROP "disk_total_space";
 
-ALTER TABLE "os_infos" ADD "os_process_cpu_load" DOUBLE PRECISION NOT NULL,
-ALTER TABLE "os_infos" ADD "os_system_load_average" DOUBLE PRECISION NOT NULL,
-ALTER TABLE "os_infos" ADD "os_system_cpu_load" DOUBLE PRECISION NOT NULL,
-ALTER TABLE "os_infos" ADD "os_total_physical_memory_size" BIGINT NOT NULL,
-ALTER TABLE "os_infos" ADD "os_free_physical_memory_size" BIGINT NOT NULL,
-ALTER TABLE "os_infos" ADD "os_free_swap_space_size" BIGINT NOT NULL,
-ALTER TABLE "os_infos" ADD "os_total_swap_space_size" BIGINT NOT NULL
+ALTER TABLE "os_infos" ADD "os_process_cpu_load" DOUBLE PRECISION NOT NULL;
+ALTER TABLE "os_infos" ADD "os_system_load_average" DOUBLE PRECISION NOT NULL;
+ALTER TABLE "os_infos" ADD "os_system_cpu_load" DOUBLE PRECISION NOT NULL;
+ALTER TABLE "os_infos" ADD "os_total_physical_memory_size" BIGINT NOT NULL;
+ALTER TABLE "os_infos" ADD "os_free_physical_memory_size" BIGINT NOT NULL;
+ALTER TABLE "os_infos" ADD "os_free_swap_space_size" BIGINT NOT NULL;
+ALTER TABLE "os_infos" ADD "os_total_swap_space_size" BIGINT NOT NULL;
