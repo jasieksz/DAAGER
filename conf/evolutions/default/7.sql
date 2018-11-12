aa5d830949ecc437642ccaa5ca20de90cef07353
Bartosz Radzynski <bradzynski@virtuslab.com>
2018-11-12 18:19:28 +0100

    DAAGER-58 Fixing not marking active

--- conf/evolutions/default/7.sql
+++ conf/evolutions/default/7.sql
@@ -2,7 +2,7 @@
 
 create table clusters(
   cluster_id TEXT NOT NULL PRIMARY KEY,
-  alias TEXT NOT NULL UNIQUE,
+  alias TEXT NOT NULL,
   base_address TEXT NOT NULL,
   is_active BOOLEAN NOT NULL
 )
